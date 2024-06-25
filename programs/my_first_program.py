a0f074450ebd8c344b3dda950f42ade491765650
ShreeHarsha-22 <shreeharshacsw@gmail.com>
2024-06-25 16:02:30 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,30 @@
 from nada_dsl import *
 
-
 def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    
+    nr_voters = 5
+    nr_candidates = 3
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    voters = []
+    for i in range(nr_voters):
+        voters.append(Party(name="Voter" + str(i)))
+    outparty = Party(name="OutParty")
 
-    return [Output(my_int1, "my_output", party1)]
+    votes_per_candidate = []
+    for c in range(nr_candidates):
+        votes_per_candidate.append([])
+        for v in range(nr_voters):
+            votes_per_candidate[c].append(SecretUnsignedInteger(Input(name="v" + str(v) + "_c" + str(c), party=voters[v])))
+
+    results = []
+    for c in range(nr_candidates):
+        total_votes = votes_per_candidate[c][0]
+        for v in range(1, nr_voters):
+            total_votes += votes_per_candidate[c][v]
+
+        total_votes_percentage = total_votes / nr_voters  # Example of adding more computation
+
+        results.append(Output(total_votes, "final_vote_count_c" + str(c), outparty))
+        results.append(Output(total_votes_percentage, "final_vote_percentage_c" + str(c), outparty))
+
+    return results
